a3333cdd82192a3b8bc66afea202b7b2bcd83bc5
Alisson Vinícius <almeidaalisson2014@gmail.com>
2023-09-09 17:01:12 -0300

    ♻️  Refatorei apliquei o primeiro teste como phpUnit

--- src/Service/Evaluator.php
+++ src/Service/Evaluator.php
@@ -6,14 +6,15 @@ use Alura\Auction\Model\Auction;
 
 class Evaluator
 {
-    private float $highestValue;
+    private float $highestValue = 0;
 
     public function avalia(Auction $auction): void
     {
-        $bids = $auction->getBids();
-        $lastPosition = count($bids) - 1;
-        $lastMove = $bids[$lastPosition];
-        $this->highestValue = $lastMove->getValue();
+        $bidValues = array_map(function ($bid) {
+            return $bid->getValue();
+        }, $auction->getBids());
+
+        $this->highestValue = max($bidValues);
     }
 
     public function getGreaterValue(): float
