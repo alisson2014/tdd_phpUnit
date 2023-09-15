bdb26c5e5be794b13c8601834f54eee80e9448f3
Alisson Vinícius <almeidaalisson2014@gmail.com>
2023-09-15 20:44:22 -0300

    ✨ Aplicando dataProvider e refinando os testes

--- tests/Service/EvaluatorTest.php
+++ tests/Service/EvaluatorTest.php
@@ -7,56 +7,103 @@ use Alura\Auction\Model\Bid;
 use Alura\Auction\Model\User;
 use Alura\Auction\Service\Evaluator;
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
 
 class EvaluatorTest extends TestCase
 {
-    public function testReturnHighestValue()
+    #[DataProvider('auctionInRandomOrder')]
+    #[DataProvider('auctionInAscendingOrder')]
+    #[DataProvider('auctionInDescendingOrder')]
+    public function testReturnLowerValue(Auction $auction)
     {
-        // Arrange - Given
-        $auction = new Auction('Fiat 147 0KM');
+        $auctioneer = new Evaluator();
 
-        $maria = new User('Maria');
-        $joao = new User('João');
+        // Act - When
+        $auctioneer->evaluate($auction);
+        $lowerValue = $auctioneer->getLowerValue();
 
-        $auction->receivesBid(new Bid($maria, 2500));
-        $auction->receivesBid(new Bid($joao, 17000));
-        $auction->receivesBid(new Bid($joao, 10000));
-        $auction->receivesBid(new Bid($joao, 1040));
-        $auction->receivesBid(new Bid($joao, 12000));
+        // Assert - Then
+        self::assertEquals(1700, $lowerValue);
+    }
 
+    #[DataProvider('auctionInRandomOrder')]
+    #[DataProvider('auctionInAscendingOrder')]
+    #[DataProvider('auctionInDescendingOrder')]
+    public function testReturnHighestValue(Auction $auction)
+    {
         $auctioneer = new Evaluator();
 
         // Act - When
         $auctioneer->evaluate($auction);
         $highestValue = $auctioneer->getGreaterValue();
-        $lowerValue = $auctioneer->getLowerValue();
 
         // Assert - Then
-        self::assertEquals(1040, $lowerValue);
-        self::assertEquals(17000, $highestValue);
+        self::assertEquals(2500, $highestValue);
     }
 
-    public function testFindHighestBids()
+    #[DataProvider('auctionInRandomOrder')]
+    #[DataProvider('auctionInAscendingOrder')]
+    #[DataProvider('auctionInDescendingOrder')]
+    public function testFindHighestBids(Auction $auction)
     {
-        $auction = new Auction('Fiat 147 0KM');
-
-        $joao = new User('João');
-        $maria = new User('Maria');
-        $ana = new User('Ana');
-        $jorge = new User('Jorge');
-
-        $auction->receivesBid(new Bid($ana, 1500));
-        $auction->receivesBid(new Bid($joao, 1000));
-        $auction->receivesBid(new Bid($maria, 2000));
-        $auction->receivesBid(new Bid($jorge, 1700));
-
         $auctioneer = new Evaluator();
         $auctioneer->evaluate($auction);
 
         $highestBids = $auctioneer->getHighestBids();
         self::assertCount(3, $highestBids);
-        self::assertEquals(2000, $highestBids[0]);
-        self::assertEquals(1700, $highestBids[1]);
-        self::assertEquals(1500, $highestBids[2]);
+        self::assertEquals(2500, $highestBids[0]);
+        self::assertEquals(2000, $highestBids[1]);
+        self::assertEquals(1700, $highestBids[2]);
+    }
+
+    public static function auctionInAscendingOrder(): array
+    {
+        $auction = new Auction('Fiat 147 0km');
+
+        $maria = new User('Maria');
+        $joao = new User('Joao');
+        $ana = new User('Ana');
+
+        $auction->receivesBid(new Bid($ana, 1700));
+        $auction->receivesBid(new Bid($joao, 2000));
+        $auction->receivesBid(new Bid($maria, 2500));
+
+        return [
+            [$auction]
+        ];
+    }
+
+    public static function auctionInDescendingOrder(): array
+    {
+        $auction = new Auction('Fiat 147 0km');
+
+        $maria = new User('Maria');
+        $joao = new User('Joao');
+        $ana = new User('Ana');
+
+        $auction->receivesBid(new Bid($maria, 2500));
+        $auction->receivesBid(new Bid($joao, 2000));
+        $auction->receivesBid(new Bid($ana, 1700));
+
+        return [
+            [$auction]
+        ];
+    }
+
+    public static function auctionInRandomOrder(): array
+    {
+        $auction = new Auction('Fiat 147 0km');
+
+        $maria = new User('Maria');
+        $joao = new User('Joao');
+        $ana = new User('Ana');
+
+        $auction->receivesBid(new Bid($joao, 2000));
+        $auction->receivesBid(new Bid($maria, 2500));
+        $auction->receivesBid(new Bid($ana, 1700));
+
+        return [
+            [$auction]
+        ];
     }
 }
